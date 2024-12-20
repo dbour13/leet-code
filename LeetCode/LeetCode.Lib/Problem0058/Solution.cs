6e06c58ccd61adabae524c5056e917b78ab3e094
Daniel Bourke <dbour13@yahoo.com.au>
2024-12-20 12:28:58 +1000

    Added solution to problem 58

---
+++ LeetCode/LeetCode.Lib/Problem0058/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+
+namespace LeetCode.Lib.Problem0058
+{
+    /// <summary>
+    /// Level: Easy
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// Given a string <paramref name="s"/> consisting of words and spaces, return the length of the last word in the string.
+        /// 
+        /// A word is a maximal substring consisting of non-space characters only.
+        /// </summary>
+        /// <param name="s">string to find length of last word</param>
+        /// <returns>Length of last word in <paramref name="s"/></returns>
+        public int LengthOfLastWord(string s)
+        {
+            // Stores the result
+            int result = 0;
+            // Whether or not we've hit a character other than a space yet
+            bool hitNonSpaceCharacter = false;
+
+            // Loop backwards through s
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                // If it isn't a space
+                if (s[i] != ' ')
+                {
+                    // Set the hitCharacter flag and increment
+                    // the result
+                    hitNonSpaceCharacter = true;
+                    result++;
+                }
+                else if (hitNonSpaceCharacter) 
+                {
+                    // Otherwise break out if it was a space and
+                    // we've already hit a non-space character before
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCode.Test/Problem0058/SolutionUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Lib;
+using LeetCode.Lib.Common;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Test.Problem0058
+{
+    public class SolutionUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int result = new Lib.Problem0058.Solution().LengthOfLastWord("Hello World");
+
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = new Lib.Problem0058.Solution().LengthOfLastWord("   fly me   to   the moon  ");
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = new Lib.Problem0058.Solution().LengthOfLastWord("luffy is still joyboy");
+
+            Assert.Equal(6, result);
+        }
+    }
+}
