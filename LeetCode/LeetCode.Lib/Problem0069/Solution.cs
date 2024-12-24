4579ea14224eb2c9533a097f11f8c5870234481e
Daniel Bourke <dbour13@yahoo.com.au>
2024-12-24 10:58:18 +1000

    Added solution to problem 69

---
+++ LeetCode/LeetCode.Lib/Problem0069/Solution.cs
@@ -0,0 +1,42 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode.Lib.Problem0069
+{
+    /// <summary>
+    /// Level: Easy
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// Given a non-negative integer <paramref name="x"/>, return the square root of <paramref name="x"/> rounded down to the nearest integer. 
+        /// The returned integer should be non-negative as well.
+        /// 
+        /// You must not use any built-in exponent function or operator.
+        /// 
+        /// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+        /// </summary>
+        /// <param name="x">Integer to find the square root of</param>
+        /// <returns>The square root of <paramref name="x"/>, rounded down</returns>
+        public int MySqrt(int x)
+        {
+            if (x <= 1)
+            {
+                return x;
+            }
+
+            // Square all numbers from 1 up to x until it creates a number
+            // greater than x
+            for (uint i = 1; i <= x; i++)
+            {
+                if (i*i > x)
+                {
+                    // Return i-1 (round down)
+                    return (int)i - 1;
+                }
+            }
+
+            // It should never reach here
+            throw new Exception();
+        }
+    }
+}
---
+++ LeetCode/LeetCode.Test/Problem0069/SolutionUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Lib;
+using LeetCode.Lib.Common;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Test.Problem0069
+{
+    public class SolutionUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int result = new Lib.Problem0069.Solution().MySqrt(4);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int result = new Lib.Problem0069.Solution().MySqrt(8);
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int result = new Lib.Problem0069.Solution().MySqrt(2147483647);
+
+            Assert.Equal(46340, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int result = new Lib.Problem0069.Solution().MySqrt(3);
+
+            Assert.Equal(1, result);
+        }
+    }
+}
